7aa28276c7687dce1d9842d001ff3243edcaee57
yegonjapheth <58904900+yegonjapheth@users.noreply.github.com>
2022-11-14 00:06:46 +0300

    Add files via upload
    working on turtles

---
+++ Moon.py
@@ -0,0 +1,12 @@
+from turtle import *
+import turtle as tur
+tur.width(10)
+tur.Screen()
+tur.bgcolor("black")
+tur.color("red")
+tur.fillcolor("yellow")
+tur.begin_fill()
+tur.circle(300)
+tur.end_fill()
+tur.hideturtle()
+tur.mainloop()
